5c05d69094a8bf09130579606a9664ebbb11d20b
Sztorm <marcinwicher99@gmail.com>
2024-08-11 11:34:44 +0200

    Added tests.

---
+++ src/jvmTest/kotlin/com/sztorm/libtest/Test.kt
@@ -0,0 +1,19 @@
+package com.sztorm.libtest
+
+import kotlin.test.Test
+
+class Test {
+    @Test
+    fun testAll() {
+        val example = ExampleClass(2, 3f)
+        val example2 = ExampleClass2(4, 5f)
+        val example3 = exampleFunction(1f)
+        val example4 = exampleFunction2(2f)
+
+        println(example)
+        println(example2)
+        println(example3)
+        println(example4)
+        assert(true)
+    }
+}
